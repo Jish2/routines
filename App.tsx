b9cc5ecaa79394bea4d802892fef5d79cce37321
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-19 23:30:42 -0500

    test changes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+const AlertButton = styled.TouchableOpacity`
+	background-color: pink;
+	padding: 16px 32px;
+	border-radius: 16px;
+`;
+
+const Container = styled.View`
+	flex: 1;
+	background-color: #fff;
+	align-items: center;
+	justify-content: center;
+	gap: 8px;
+`;
+
+const example = [1, 3, 4, 2, 14, 15];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [count, setCount] = useState<number>(0);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	useEffect(() => {
+		if (count % 5 == 0 && count != 0) Alert.alert("Button with adjusted color pressed");
+	}, [count]);
+
+	return (
+		<Container>
+			{example.map((item, i) => {
+				return <Text key={i}>{item}</Text>;
+			})}
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<Text>count: {count}</Text>
+
+			<AlertButton onPress={() => setCount((p) => p + 1)}>
+				<Text>testing 123</Text>
+			</AlertButton>
+			<StatusBar style="auto" />
+		</Container>
+	);
+}
